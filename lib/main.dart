f6ab5973fc8c1e8b3c32d69ae8c6ecb301500355
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-03-31 15:16:48 -0600

    Cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flighttickets_app/CustomAppBar.dart';
 import 'package:flighttickets_app/CustomShapeClipper.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -27,9 +29,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: CustomAppBottomBar(),
       body: Column(
         children: <Widget>[
           HomeScreenTopPart(),
+          homeScreenBottomPart,
         ],
       ),
     );
@@ -201,6 +205,170 @@ class _ChoiceChipState extends State<ChoiceChip> {
             widget.text,
             style: TextStyle(color: Colors.white, fontSize: 16.0),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+var viewAllStyle = TextStyle(fontSize: 14.0, color: appTheme.primaryColor);
+var homeScreenBottomPart = Column(
+  children: <Widget>[
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            "Currently Watched Items",
+            style: dropDownMenuItemStyle,
+          ),
+          Spacer(),
+          Text(
+            "VIEW ALL(12)",
+            style: viewAllStyle,
+          ),
+        ],
+      ),
+    ),
+    Container(
+      height: 250.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: cityCards,
+      ),
+    ),
+  ],
+);
+
+List<CityCard> cityCards = [
+  CityCard(
+      "assets/images/lasvegas.jpg", "Las Vegas", "Feb 2019", "45", 4299, 2250),
+  CityCard("assets/images/athens.jpg", "Athens", "Apr 2019", "50", 9999, 4151),
+  CityCard("assets/images/sydney.jpeg", "Sydney", "Dec 2018", "40", 5999, 399),
+];
+final formatCurrency = NumberFormat.simpleCurrency();
+
+class CityCard extends StatelessWidget {
+  final String imagePath, cityName, monthYear, discount;
+  final int oldPrice, newPrice;
+
+  CityCard(this.imagePath, this.cityName, this.monthYear, this.discount,
+      this.oldPrice, this.newPrice);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: 210.0,
+                  width: 160.0,
+                  child: Image.asset(
+                    imagePath,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  left: 0.0,
+                  bottom: 0.0,
+                  width: 160.0,
+                  height: 60.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                          Colors.black,
+                          Colors.black.withOpacity(0.1),
+                        ])),
+                  ),
+                ),
+                Positioned(
+                  left: 10.0,
+                  bottom: 10.0,
+                  right: 10.0,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            '$cityName',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 18.0),
+                          ),
+                          Text(
+                            '$monthYear',
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                                fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 6.0, vertical: 2.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        child: Text(
+                          '$discount%',
+                          style: TextStyle(fontSize: 14.0, color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                width: 5.0,
+              ),
+              Text(
+                '${formatCurrency.format(newPrice)}',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0),
+              ),
+              SizedBox(
+                width: 5.0,
+              ),
+              Text(
+                "(${formatCurrency.format(oldPrice)})",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12.0),
+              ),
+            ],
+          )
         ],
       ),
     );
